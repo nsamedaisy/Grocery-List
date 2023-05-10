fdf37f556b9f4e4b0875f339a88923ab235fc38c
nsamedaisy <nsamedaisy@gmail.com>
2023-05-10 14:58:05 +0100

    fixed an equal sign in js

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-// const groceries = document.getElementsByClassName("groceries");
+const pencil = document.getElementById('pencil')
+const allItems = document.getElementById('all-items')
+const userInput = document.getElementById('user-input')
 
-const pencil = document.getElementById('pencil');;
-const allItems = document.getElementById('all-items');;
-const userInput = document.getElementById('user-input');;
+pencil.addEventListener('click', function () {
+  allItems.innerHTML = ''
+})
 
-pencil.addEventListener('click', function() {
-  allItems.innerHTML = '';;
-});
-
-userInput.addEventListener('keydown', function(event) {
-  if(event.key == 'Enter') {
-	addItem();
+userInput.addEventListener('keydown', function (event) {
+  if (event.key === 'Enter') {
+    addItem()
   }
-});
+})
 
-function addItem() {
-  const h2 = document.createElement('h2');
-  h2.innerHTML = '- ' + userInput.value;;
+function addItem () {
+  const h2 = document.createElement('h2')
+  h2.innerHTML = '# ' + userInput.value
 
-  h2.addEventListener ('click', function() {
-    h2.style.textDecoration = 'line-through';;
-  });
+  h2.addEventListener('click', function () {
+    h2.style.textDecoration = 'line-through'
+  })
 
-  allItems.insertAdjacentElement('beforeend', h2);;
+  allItems.insertAdjacentElement('beforeend', h2)
 
-  userInput.value = '';;
+  userInput.value = ''
 };
